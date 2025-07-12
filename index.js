828f8549d94f50e72355210b800de8572e4b19b5
AnthonyMacks <56456892+AnthonyMacks@users.noreply.github.com>
2025-07-12 18:36:30 +1000

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.post("/relay", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Relay server running on port ${PORT}`);
 });
