2ea2e81ed914b405461598086bbb1f1a7678d479
AnthonyMacks <56456892+AnthonyMacks@users.noreply.github.com>
2025-07-27 12:19:00 +1000

    Update gps-relay.py

--- gps-relay.py
+++ gps-relay.py
@@ -49,5 +49,5 @@ def health():
     return "🟢 Fly relay is up and forwarding GPS data."
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 8080))
+    PORT = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=PORT)
